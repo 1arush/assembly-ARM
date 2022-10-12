136e87cd27fc6513a882415103b589befa3b8be1
Rushil Khullar <105356056+sayRushil@users.noreply.github.com>
2022-10-12 16:42:37 +0530

    Basic Operations in ARM
    Week-1

---
+++ operationsALU.s
@@ -0,0 +1,19 @@
+mov r0,#0;
+mov r1,#1;
+mov r2,#2;
+mov r3,#3;
+mov r4,#4;
+mov r5,#5;
+mov r6,#6;
+mov r7,#7;
+mov r8,#8;
+mov r9,#9;
+add r10,r0,r1;
+add r10,r10,r2;
+add r10,r10,r3;
+add r10,r10,r4;
+
+mul r11,r10,r6;
+
+sub r12,r2,r4;
+
